a2a66155313b18ad1e12ef05dd03fb11d6e5cd41
Sergio Moreno Fdez <sergiomorenofdez@gmail.com>
2024-07-22 10:42:47 +0200

    Create Srgodzy.php
    Tipo de variables

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/php/Srgodzy.php
@@ -0,0 +1,15 @@
+<?php
+
+
+/*TIPOS DE VARIABLES*/ 
+
+$integer = 1; // Integer: Números enteros.
+$float = 18.21; // Float: Números decimales.
+$boolean = true; // Boolean: Valores lógicos (true o false).
+$null = null; // Null: Dato nulo "no valor".
+$string = 'Cadena de texto'; // String: Cadenas de texto o caracteres únicos.
+$arrays = array('a', 'b', 'c'); // Array: Almacenar varios datos en un mismo lugar.
+
+// echo es impresion de texto en pantalla
+echo "¡Hola Mundo";
+?>
